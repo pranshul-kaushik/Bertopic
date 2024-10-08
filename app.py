ac99dfa4b40674401df1bf3e4ff62d85ec47fcc6
Pranshul <pranshul47007@gmail.com>
2024-10-08 14:28:00 +0530

    bug fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 
 from src.scripts.nlp_processing import embed_splitted_docs, split_corpus
 from src.scripts.topic_modeling import topic_modeling
-from src.utils.huggingface_utlis import save_dataset_to_hf_hub, save_model_to_hf_hub
+from src.utils.huggingface_utlis import save_dataset_to_hf_hub
 from src.utils.utils import extract_corpus
 
 
